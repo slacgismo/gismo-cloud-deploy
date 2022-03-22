78695ef2f1dbf3874681636d442a18b33e426443
Catzzz <chuneleu@gmail.com>
2022-03-22 11:23:51 -0700

    database transations

--- gismoclouddeploy/server/project/users/views.py
+++ gismoclouddeploy/server/project/users/views.py
@@ -1,14 +1,17 @@
 import random
 import logging
+from string import ascii_lowercase  # new
 
 import requests
 from celery.result import AsyncResult
 from flask import Blueprint, render_template, flash, abort, request, Response, jsonify, current_app
 
 from . import users_blueprint
-from project import csrf
+from project import csrf, db  # updated
 from project.users.forms import YourForm
-from project.users.tasks import sample_task, task_process_notification
+from project.users.models import User  # new
+from project.users.tasks import sample_task, task_process_notification, task_send_welcome_email  # updated
+
 
 def api_call(email):
     # used for testing a failed api call
@@ -76,3 +79,32 @@ def subscribe_ws():
             'task_id': task.task_id,
         })
     return render_template('form_ws.html', form=form)
+
+def random_username():
+    username = ''.join([random.choice(ascii_lowercase) for i in range(5)])
+    return username
+
+@users_blueprint.route('/transaction_celery/', methods=('GET', 'POST'))
+@csrf.exempt
+def transaction_celery():
+    try:
+        username = random_username()
+        user = User(
+            username=f'{username}',
+            email=f'{username}@test.com',
+        )
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        raise
+
+    current_app.logger.info(f'***** - user {user.id} {user.username} is persistent now')
+    # users = User.query.all()
+    # for user in users:
+    #     current_app.logger.info(f'user email:  {user.email} userid: {user.id}')
+    # return "done"
+    task = task_send_welcome_email.delay(user.id)
+    return jsonify({
+            'task_id': task.task_id,
+    })
