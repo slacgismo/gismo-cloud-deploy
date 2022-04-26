d2ba13f2e49bfcba71c2fca6a7048c041fab44a3
JimmyLeu76 <jimmyleu@slac.stanford.edu>
2022-04-26 04:32:05 -0700

    update task status

--- gismoclouddeploy/services/client/utils/InvokeFunction.py
+++ gismoclouddeploy/services/client/utils/InvokeFunction.py
@@ -29,7 +29,7 @@ def invok_docekr_exec_run_process_file( bucket_name,
         # get pod name
        
         pod_name = get_k8s_pod_name(container_name)
-        print(f" k8s pod_name: {pod_name}")
+        # print(f" k8s pod_name: {pod_name}")
         # print(f"pod_name: {pod_name}")
         command = [ "kubectl", 
                     "exec",
@@ -53,20 +53,38 @@ def invok_docekr_exec_run_process_file( bucket_name,
 
 
 
-def invoke_docker_exec_get_task_status(task_id):
-    command = ["docker", 
-    "exec",
-    "-it",
-     "web",
-     "python",
-     "app.py",
-     "get_task_status",
-    f"{task_id}"
-     ]
-    res = exec_docker_command(command)
-    r2 = res.replace("\'","\"")
-    print(f"res here--->: {r2}")
-    return res
+def invoke_docker_exec_get_task_status(task_id,container_type,container_name ):
+    if container_type == "docker":
+        command = ["docker", 
+        "exec",
+        "-it",
+        container_name,
+        "python",
+        "app.py",
+        "get_task_status",
+        f"{task_id}"
+        ]
+        res = exec_docker_command(command)
+        r2 = res.replace("\'","\"")
+        print(f"res here--->: {r2}")
+        return res
+    elif container_type == "kubernetes":
+        pod_name = get_k8s_pod_name(container_name)
+        command = [ "kubectl", 
+                    "exec",
+                    pod_name,
+                    "--stdin",
+                    "--tty",
+                    "--",
+                    "python",
+                    "app.py",
+                     "get_task_status",
+                    f"{task_id}"
+                ]
+        res = exec_docker_command(command)
+        r2 = res.replace("\'","\"")
+        print(f"res here--->: {r2}")
+        return res
 
 def exec_docker_command(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
