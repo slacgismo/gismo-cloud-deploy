823818a53a048c33fba8404750f4d588ebbda77c
JimmyLeu76 <jimmyleu@slac.stanford.edu>
2022-04-26 16:01:09 -0700

    update k8s and update servers function of save files

--- gismoclouddeploy/services/server/project/solardata/tasks.py
+++ gismoclouddeploy/services/server/project/solardata/tasks.py
@@ -15,9 +15,21 @@ from project.solardata.models import SolarData
 from io import StringIO
 
 
-
-
 logger = get_task_logger(__name__)
+
+@shared_task()
+def combine_files_to_file_task(bucket_name,source_folder,target_folder,target_filename):
+    response_object = {
+        'status': 'success',
+        'container_id': os.uname()[1]
+    }
+    from project import create_app
+    from project.solardata.models import SolarData
+    from project.solardata.utils import combine_files_to_file
+    app = create_app()
+    with app.app_context():
+        response = combine_files_to_file(bucket_name, source_folder, target_folder, target_filename)
+        return response
 
 @shared_task()
 def read_all_datas_from_solardata():
@@ -30,14 +42,13 @@ def read_all_datas_from_solardata():
 
     app = create_app()
     with app.app_context():
-        solardata = [solardata.to_json() for solardata in SolarData.query.all()]
+        solardata = [solardata.to_json()
+                     for solardata in SolarData.query.all()]
         return solardata
 
 
-
-   
 @shared_task()
-def save_data_from_db_to_s3_task(bucket_name,file_path,file_name, delete_data):
+def save_data_from_db_to_s3_task(bucket_name, file_path, file_name, delete_data):
     response_object = {
         'status': 'success',
         'container_id': os.uname()[1]
@@ -45,20 +56,21 @@ def save_data_from_db_to_s3_task(bucket_name,file_path,file_name, delete_data):
     from project import create_app
     from project.solardata.models import SolarData
     from project.solardata.utils import to_s3
-    app = create_app() 
+    app = create_app()
     with app.app_context():
-        solardatas = [solardata.to_json() for solardata in SolarData.query.all()]
+        solardatas = [solardata.to_json()
+                      for solardata in SolarData.query.all()]
         # logger.info(solardatas)
-   
+
         # convert josn to csv file and save to s3
         # current_app.logger.info(all_datas)
         df = pd.json_normalize(solardatas)
-        csv_buffer=StringIO()
+        csv_buffer = StringIO()
         df.to_csv(csv_buffer)
         content = csv_buffer.getvalue()
         try:
-            logger.info(bucket_name,file_path,file_name)
-            to_s3(bucket_name,file_path,file_name, content)
+            logger.info(bucket_name, file_path, file_name)
+            to_s3(bucket_name, file_path, file_name, content)
         except Exception as e:
             response_object = {
                 'status': 'failed',
@@ -68,11 +80,8 @@ def save_data_from_db_to_s3_task(bucket_name,file_path,file_name, delete_data):
         return response_object
 
 
-
-
-
 @shared_task(bind=True)
-def process_data_task(self, bucket_name,file_path,file_name,column_name,start_time,solver):
+def process_data_task(self, bucket_name, file_path, file_name, column_name, start_time, solver, saved_bucket, saved_file_path, saved_filename):
     response_object = {
         'status': 'success',
         'container_id': os.uname()[1]
@@ -82,13 +91,9 @@ def process_data_task(self, bucket_name,file_path,file_name,column_name,start_ti
     from project.solardata.utils import (
         process_solardata_tools
     )
-    app = create_app() 
+    app = create_app()
     with app.app_context():
 
-        process_solardata_tools(bucket_name,file_path,file_name,column_name,solver,start_time, self.request.id)
-        return "reutn from process solardatatools"
-     
-
-
-
-
+        process_solardata_tools(bucket_name, file_path, file_name, column_name, solver,
+                                start_time, self.request.id, saved_bucket, saved_file_path, saved_filename)
+        return "return  from process solardatatools"
