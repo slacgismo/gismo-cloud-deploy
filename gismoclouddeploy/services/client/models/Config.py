325f494aad655c02ef3c961c1e02073db4ee980d
JimmyLeu76 <jimmyleu@slac.stanford.edu>
2022-04-27 11:14:30 -0700

    update config.yaml add interval check status

--- gismoclouddeploy/services/client/models/Config.py
+++ gismoclouddeploy/services/client/models/Config.py
@@ -12,8 +12,11 @@ class Config(object):
                  saved_target_filename,
                  environment,
                  container_type,
-                 container_name
+                 container_name,
+                 interval_of_check_task_status,
+                 interval_of_exit_check_status,
                  ):
+
         self.files = files
         self.bucket = bucket
         self.column_names = column_names
@@ -24,6 +27,9 @@ class Config(object):
         self.environment = environment
         self.container_type = container_type
         self.container_name = container_name
+        self.interval_of_check_task_status = interval_of_check_task_status
+        self.interval_of_exit_check_status = interval_of_exit_check_status
+
     
     def import_config_from_yaml(file):
         config_params = read_yaml(file)
@@ -37,7 +43,10 @@ class Config(object):
             saved_target_filename = config_params["output"]["saved__target_filename"],
             environment = config_params["general"]["environment"],
             container_type = config_params["general"]["container_type"],
-            container_name = config_params["general"]["container_name"])
+            container_name = config_params["general"]["container_name"],
+            interval_of_check_task_status = config_params["general"]["interval_of_check_task_status"],
+            interval_of_exit_check_status = config_params["general"]["interval_of_exit_check_status"]
+            )
         return config
 
 # gen_config =  read_yaml("./config/general.yaml")
---
+++ gismoclouddeploy/services/client/models/Task.py
@@ -0,0 +1,13 @@
+class Task(object):
+
+    def __init__(self,
+                task_id,
+                task_status,
+                task_result,
+                ):
+        self.task_id = task_id
+        self.task_status = task_status
+        self.task_result = task_result
+    
+
+
