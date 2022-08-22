8b3a3ea38b7c9e5f7733470df1a6c3be49ef0d9f
Catzzz <chuneleu@gmail.com>
2022-08-22 12:28:34 -0700

    change back deployment , update CELERYD_PREFETCH_MULTIPLIER

--- gismoclouddeploy/services/server/project/config.py
+++ gismoclouddeploy/services/server/project/config.py
@@ -9,15 +9,16 @@ class BaseConfig:
 
     TESTING = False
 
-    broker_url = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
-    result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
+    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
+    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
     broker_connection_retry = True  #  retry whenever it fails
     broker_connection_max_retries = 0  # disable the retry limit.
     task_soft_time_limit = True
     task_track_started = True
     task_annotations = {"*": {"rate_limit": "10/s"}}
     task_acks_late = True
-    worker_prefetch_multiplier = 1
+    # worker_prefetch_multiplier = 1
+    CELERYD_PREFETCH_MULTIPLIER = 64
 
 
 class DevelopmentConfig(BaseConfig):
