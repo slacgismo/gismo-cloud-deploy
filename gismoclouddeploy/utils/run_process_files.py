3b0ef2a32f53f8c3340e85089984fa22712d6b46
Catzzz <chuneleu@gmail.com>
2022-09-25 13:47:06 -0700

    fix copy license to dockerfile

--- gismoclouddeploy/utils/run_process_files.py
+++ gismoclouddeploy/utils/run_process_files.py
@@ -108,7 +108,7 @@ def run_process_files(
             # something wrong break while loop and clean services. 
             logging.error(f"Somehting wrong : {e}")
             break
-           
+
     # clean up state, clean up k8s, delete namspaces, scale down eks nodes to 0 .
     logging.info(f" ===== State: {gcd.state} =======")
     gcd.trigger_cleanup()
